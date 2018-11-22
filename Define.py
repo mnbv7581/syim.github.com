96deda5b00bc06ee9de1ef9aa3bf78b18689559e
mnbv7581 <44501825+mnbv7581@users.noreply.github.com>
2018-11-22 15:38:43 +0900

    CNN Layer - VGG
    VGG 레이어 구현 소스

---
+++ Define.py
@@ -0,0 +1,16 @@
+DROP_OUT = 0.5
+
+IMAGE_CHANNEL = 3
+IMAGE_WIDTH = 32
+IMAGE_HEIGHT = 32
+
+CLASSES = 10
+
+MAX_EPOCHS = 300
+BATCH_SIZE = 128
+
+#CIFAR10 - VGG
+filters = [16, 32, 64, 128]
+fc_parameters = [128, 64]
+
+CLASS_NAME = ['0','1','2','3','4','5','6','7','8','9']
