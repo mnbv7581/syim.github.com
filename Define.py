4c21e11e5398d58a514261d5436e02d7748a529e
mnbv7581 <44501825+mnbv7581@users.noreply.github.com>
2018-11-22 15:58:48 +0900

    Update Define.py

--- Define.py
+++ Define.py
@@ -13,4 +13,4 @@ BATCH_SIZE = 128
 filters = [16, 32, 64, 128]
 fc_parameters = [128, 64]
 
-CLASS_NAME = ['0','1','2','3','4','5','6','7','8','9']
+CLASS_NAME = ['airplane','car','bird','cat','fawn','dog','frog','horse','boat','truck']
